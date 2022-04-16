cd1c5cf4b2a0c0c66c04dc08617ae917182d66f2
LR <liurui990312@163.com>
2022-04-16 17:46:47 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @Author:LR
+ * @Email:2916117847@qq.com
+ * @Create:2022年04月16日17:30
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test");
+    }
+}
